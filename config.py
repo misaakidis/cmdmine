b827f5aa5de669abd9eccf64d2ef88dada0e8671
Zack Mullaly <redwire@riseup.net>
2015-07-05 16:45:36 -0400

    Added some nice comments to explain fields and a section for mapping activities

--- config.py
+++ config.py
@@ -1,7 +1,28 @@
+# URL of your redmine instance
 REDMINE_LOCATION = ''
+
+# Your redmine username
 REDMINE_USERNAME = ''
+
+# Your redmine password
 REDMINE_PASSWORD = ''
 
-# Mapping of aliases to project names
+# Mapping of shorthand aliases to project names
+# For example, for a project named 'internal-management-tool', you could have an entry
+# 'intmanage': 'internal-management-tool'
+# Remember to separate entries with commas!
 REDMINE_PROJECTS = {
+}
+
+# A Mapping of activity names to their identifiers
+# You can create an alias or provide whatever name you want for an activity
+# as long as you map to the same ID. For example, you could have another entry
+# 'dev': 9
+# to provide a shorthand way of referring to development activity
+ACTIVITIES = {
+  'design': 8,
+  'development': 9,
+  'maintenance': 10,
+  'progress': 11,
+  'communications': 12
 }
